26d02cbb631a827f465a2b45b74cace975223d94
toni <antoniojsereno@gmail.com>
2023-02-04 13:37:13 -0300

    Adicao do Popover usando headless ui

---
+++ web/src/App.tsx
@@ -0,0 +1,5 @@
+import { Widget } from "./Componentes/Widget";
+
+export function App(){
+  return <Widget></Widget>;
+}
---
+++ web/tailwind.config.cjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,tsx}"],
+  theme: {
+    extend: {
+      colors:{ 
+        brand:{
+          500: '#8257e6',
+        }
+       }
+    },
+  },
+  plugins: [
+    
+  ],
+}
